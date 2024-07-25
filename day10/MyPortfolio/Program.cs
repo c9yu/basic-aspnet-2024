0952e2eb1cb485458356b6a438a2a6172d5ad876
c9yu <polkj11290@naver.com>
2024-07-25 09:24:11 +0900

    20240725

--- day10/MyPortfolio/Program.cs
+++ day10/MyPortfolio/Program.cs
@@ -20,7 +20,7 @@ namespace MyPortfolio
             // 로그인 세션 설정
             builder.Services.AddSession(options =>
             {
-                options.Cookie.Name = "Hwangji99 PortfolioSession"; // 웹앱 세션 쿠키 이름
+                options.Cookie.Name = "c9yu PortfolioSession"; // 웹앱 세션 쿠키 이름
                 options.IdleTimeout = TimeSpan.FromMinutes(20); // 세션지속시간 20~30분이 적당
             }).AddControllersWithViews(); // 세션의 내용을 cshtml에도 적용한다
 
